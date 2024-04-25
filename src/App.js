84f3dbfc57d03fef0fcf14cecfce59e9c0d0ac20
arrow66 <arjunan8080@gmail.com>
2024-04-25 14:03:06 -0600

    init

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import logo from '../static/images/logo.svg'
+import '../static/css/App.css'
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    Please start your design and development
+                </p>
+            </div>
+        )
+    }
+}
+
+export default App;
